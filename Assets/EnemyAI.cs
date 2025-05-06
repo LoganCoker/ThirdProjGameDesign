2b76c3a37f158e7b0212267f9b7741020c8699ec
Zoe <zoe052202@gmail.com>
2025-05-06 16:12:55 -0500

    enemyAI

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatisGround, whatisPlayer;
+
+    public float health;
+
+    // patrolling
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // attacking
+    public float timeBetweenAtks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    // states 
+    public float sightRange, atkRange;
+    public bool playerinSightRange, playerinAtkRange;
+
+    private void Awake(){
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update(){
+        // check for sight and atk range
+        playerinSightRange = Physics.CheckSphere(transform.position, sightRange, whatisPlayer);
+        playerinAtkRange = Physics.CheckSphere(transform.position, atkRange, whatisPlayer);
+    
+        if (!playerinSightRange && !playerinAtkRange) Patrolling();
+        if (playerinSightRange && !playerinAtkRange) ChasePlayer();
+        if (playerinAtkRange && playerinSightRange) AtkPlayer();
+    }
+
+    private void Patrolling(){
+        if (!walkPointSet) SearchWalkPoint();
+
+        if(walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distancetoWalkPoint = transform.position - walkPoint;
+
+        // reached walkpoint
+        if (distancetoWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkPoint(){
+        // calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+       if (Physics.Raycast(walkPoint, -transform.up, 2f, whatisGround))
+        walkPointSet = true;
+    }
+
+
+    private void ChasePlayer(){
+        agent.SetDestination(player.position);
+    }
+
+    private void AtkPlayer(){
+        // make sure enemy doesnt move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if(!alreadyAttacked){
+
+            // attack sequence
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 8f, ForceMode.Impulse);
+
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAtk), timeBetweenAtks);
+        }
+    }
+
+    private void ResetAtk(){
+        alreadyAttacked = false;
+    }
+
+    public void TakeDmg(int dmg){
+        health -= dmg;
+
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+
+    }
+
+    private void DestroyEnemy(){
+        Destroy(gameObject);
+    }
+}
