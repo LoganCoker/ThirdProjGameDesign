bb7bb3f2b655839db2b9260b1f98003622dd7a34
Zoe <zoe052202@gmail.com>
2025-05-08 18:20:58 -0500

    stuff

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
 
     public void PlayGame(){
-        SceneManager.LoadSceneAsync("LoganTest");
+        SceneManager.LoadSceneAsync("Graveyard");
     }
 
     public void Options(){
