7564a4b38d089ea6a74478a3ead001a1b5b0dea5
Zoe <zoe052202@gmail.com>
2025-05-08 19:12:06 -0500

    fade in and out black screen

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -1,16 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     private string sceneToLoad;
+    public Image blackScreen;
+    bool loadIn = false;
 
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("CastleRoom1");
+
+            loadIn = true;
+        }
+
+
+    }
+
+    void Update(){
+        if (loadIn){
+            if (blackScreen.color.a < 1f){
+                Color temp = blackScreen.color;
+                temp.a += Time.deltaTime * 0.7f;
+                blackScreen.color = temp;
+            }
+            if (blackScreen.color.a > 0.95f)
+                SceneManager.LoadScene("CastleRoom1");
         }
     }
 }
