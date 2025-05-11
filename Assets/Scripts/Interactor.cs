546cd28912750e8ab37f1ca5e08765eaabe3f828
Zoe <zoe052202@gmail.com>
2025-05-11 16:56:28 -0500

    started interactor

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour{
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionPointRadius;
+    
+}
