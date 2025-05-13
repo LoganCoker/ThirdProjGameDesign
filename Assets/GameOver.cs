c50cd7bb0d18e0c0f68b6824ab263e9af0e08357
Zoe <zoe052202@gmail.com>
2025-05-13 16:38:49 -0500

    gameover

---
+++ Assets/GameOver.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Data;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+   [SerializeField] GameObject gameOver;
+   public AudioClip gameOverAudio;
+   private AudioSource audioSrc;
+    void Start() {
+     
+    }
+
+    public void GameOver(){
+        gameOver.SetActive(true);
+        Time.timeScale = 0;
+    }
+ 
+    public void Home(){
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+        
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
