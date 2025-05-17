51040682ba94271fa41c579ceada94b1c86bf6b9
Lucas-Alvarado <lucasalvarado615@gmail.com>
2025-05-17 18:17:55 -0500

    Main menu sound

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Audio Settings")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private float fadeOutDuration = 1.5f;
 
     public void PlayGame(){
-        SceneManager.LoadSceneAsync("Graveyard");
+        StartCoroutine(FadeOutAndLoadScene("Graveyard"));
     }
 
     public void Options(){
@@ -17,4 +21,25 @@ public class MainMenu : MonoBehaviour
    public void QuitGame(){
     Application.Quit();
    }
+
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        if (audioSource != null && audioSource.clip != null && audioSource.isPlaying)
+        {
+            float startVolume = audioSource.volume;
+            float timer = 0; 
+
+            while (timer < fadeOutDuration)
+            {
+                timer += Time.deltaTime;
+                audioSource.volume = Mathf.Lerp(startVolume, 0, timer / fadeOutDuration);
+                yield return null;
+            }
+
+            audioSource.volume = 0;
+            audioSource.Stop(); 
+        }
+
+        SceneManager.LoadSceneAsync(sceneName); 
+    }
 }
